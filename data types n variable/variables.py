1e27f60693433d9035a6eb70ce7a5eacab742b14
hp <urmee.zerin@gmail.com>
2022-05-25 17:59:15 +0600

    project setup,variables

---
+++ data types n variable/variables.py
@@ -0,0 +1,64 @@
+a = 10
+print(type(a))
+
+a =20
+print(a)
+
+b = 1.1
+print(type(b))
+
+c =123456789
+print(type(c))
+
+#variable rules- and or not for from as if else pass class in is with dle break continue except def trturn finally
+#MyName= "Zerin" pascel rule
+#my_name= zerin_sultana underscore
+#myName= zerinSultana camel class
+#variables are int, float and boolean
+
+#boolean_status= true
+#boolean_status2= false
+
+a = 10
+print(type(a))
+
+d = 'a'
+print(type(d))
+
+e = 1.1
+print(type(e))
+#String fprmating rules
+'''
+comment
+
+'''
+my_goal = "find a job"
+print(my_goal)
+my_status = "i am urmee"
+print(my_status)
+my_role = "qa"
+print(my_role)
+
+#String method
+status_upper = my_status.upper()
+print(status_upper)
+status_lower = my_status.lower()
+print(status_lower)
+my_status_and_roles = my_status+my_role
+print(my_status_and_roles)
+my_status_and_roles = my_status + my_role
+print(my_status_and_roles)
+
+
+a,b = 10, 5
+c = 'zerin'
+sum = a+b
+print(sum)
+sum = a+b+c
+print(sum)
+a = '10'
+b = '5'
+c = 'zerin'
+sum = (a+b+c)
+print(sum)
+
